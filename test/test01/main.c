467905bb2da27e599b5b2b9cbd537ea4204a02bd
Sophi-sketch <sophiamando242@gmail.com>
2025-03-19 10:39:48 -0300

    Update main.c

--- test/test01/main.c
+++ test/test01/main.c
@@ -1,5 +1,24 @@
-int main(int argc, char const *argv[])
-{
-      printf("Olá mundo");
-      return 0;
+#include <stdio.h>
+
+int main(void){
+
+      float num1, num2, num3, med;
+
+printf("Digite o 1o. numero ==> ");
+scanf("%f", &num1);
+
+printf("Digite o 2o. numero ==> ");
+scanf("%f", &num2);
+
+printf("Digite o 3o. numero ==> ");
+scanf("%f", &num3);
+
+med=(num1+num2+num3)/3;
+
+printf("\n Os numeros inseridos foram: %0.1f, %0.1f e %0.1f", num1, num2, num3, med);
+
+printf("\n\n A media dos numeros foram: %0.1f", med);
+
+return 0;
+
 }
